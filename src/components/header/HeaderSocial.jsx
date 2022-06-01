85a3405e292c79e00071c3b7a97d331120f90446
Akande Gbolahan <geakande@gmail.com>
2022-06-01 11:52:26 +0100

    Solved Social Issues

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -5,9 +5,9 @@ import { FaGithub, FaSlack } from "react-icons/fa"
 const HeaderSocial = () => {
   return (
     <div className='header__social'>
-      <a href="https://www.linkedin.com/in/gbolahan-akande-079ba814b/" target="_blank"><BsLinkedin /></a>
-      <a href="https://github.com/gboigwe" target="_blank"><FaGithub /></a>
-      <a href="https://slack.com/geakande" target="_blank"><FaSlack /></a>
+      <a href="https://www.linkedin.com/in/gbolahan-akande-079ba814b/" target="_blank" rel='noopener noreferrer' ><BsLinkedin /></a>
+      <a href="https://github.com/gboigwe" target="_blank" rel='noopener noreferrer' ><FaGithub /></a>
+      <a href="https://slack.com/geakande" target="_blank" rel='noopener noreferrer' ><FaSlack /></a>
     </div>
   )
 }
