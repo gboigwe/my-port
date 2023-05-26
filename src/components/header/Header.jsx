6514c5aa28c2a7df9ec28b3e76a71d5b7bb7c863
Akande Gbolahan <geakande@gmail.com>
2023-05-26 12:42:19 +0100

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className='container header__container'>
         <h5>Hello I'm AGE</h5>
         <h2>Akande Gbolahan Emmanuel</h2>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Web Developer</h5>
         <CTA />
         <HeaderSocial />
 
