b3dfec0d00ba0dc02ef80fb9784641e731337fdd
Akande Gbolahan <geakande@gmail.com>
2023-05-26 12:06:57 +0100

    Update HeaderSocial.jsx

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsLinkedin } from "react-icons/bs"
+import { BsLinkedin, BsPinterest } from "react-icons/bs"
 import { FaGithub, FaSlack } from "react-icons/fa"
 
 const HeaderSocial = () => {
@@ -7,7 +7,7 @@ const HeaderSocial = () => {
     <div className='header__social'>
       <a href="https://www.linkedin.com/in/gbolahan-akande-079ba814b/" target="_blank" rel='noopener noreferrer' ><BsLinkedin /></a>
       <a href="https://github.com/gboigwe" target="_blank" rel='noopener noreferrer' ><FaGithub /></a>
-      <a href="https://www.pinterest.com/geakande/" target="_blank" rel='noopener noreferrer' ><FaPinterest /></a>
+      <a href="https://www.pinterest.com/geakande/" target="_blank" rel='noopener noreferrer' ><BsPinterest /></a>
     </div>
   )
 }
