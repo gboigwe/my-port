d9ef5954f9cdf380bbfd901e822c2680a1c50472
Akande Gbolahan <geakande@gmail.com>
2023-08-12 17:41:39 +0100

    Uploaded Files
    Added My Team Project and My Elevator Pitch

---
+++ src/components/portfolio/EPA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EP from '../../assets/Elevator_Pitch.mp3'
+
+const EPA = () => {
+  return (
+    <div className='cta'>
+      <a href={EP} download className='btn'>My Pitch</a>
+    </div>
+  )
+}
+
+export default EPA
---
+++ src/components/portfolio/TPA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TP from '../../assets/Team_Project.pdf'
+
+const TPA = () => {
+  return (
+    <div className='cta'>
+      <a href={TP} download className='btn'>Team Project</a>
+    </div>
+  )
+}
+
+export default TPA
