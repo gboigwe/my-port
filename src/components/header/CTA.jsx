df3bc8488cb3866318778ae5fd3d016a0ce14a54
Akande Gbolahan <geakande@gmail.com>
2023-08-13 07:04:23 +0100

    Update CTA.jsx

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import CV from '../../assets/cvGbolahan.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={CV} download className='btn' onClick={() => alert("ALERT!!!: Please if download doesn't show in 10seconds, refresh page")}>Download CV</a>
       <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
