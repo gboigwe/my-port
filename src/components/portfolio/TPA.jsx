714f33d4d22dbc138ea99e5ec24531a211f8ee56
Akande Gbolahan <geakande@gmail.com>
2023-08-13 07:03:12 +0100

    Update TPA.jsx

--- src/components/portfolio/TPA.jsx
+++ src/components/portfolio/TPA.jsx
@@ -4,7 +4,7 @@ import TP from '../../assets/Team_Project.pdf'
 const TPA = () => {
   return (
     <div className='cta'>
-      <a href={TP} download className='btn btn-primary' onClick={() => alert("ALERT!!!: Please if download doesn't show, refresh page")}>Download Team Project</a>
+      <a href={TP} download className='btn btn-primary' onClick={() => alert("ALERT!!!: Please if download doesn't show in 10 seconds, refresh page")}>Download Team Project</a>
     </div>
   )
 }
