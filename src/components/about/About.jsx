7ff0cfdbeb33f6fe7c2f3500f32af14b33432bab
Akande Gbolahan <geakande@gmail.com>
2023-08-18 16:16:21 +0100

    Embeded Video
    I embeded a youtube link video for my pitch

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -61,6 +61,7 @@ const About = () => {
             If you are looking to build your business/brand or you need an upgrade to a better level, reach out to me and let me make you 
             achieve your business vision.
           </p>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/RAKNPGBdKoM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </section>
   )
 }
