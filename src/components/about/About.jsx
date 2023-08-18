105e7a63512af537966273f0481fc5c426d7b2d2
Akande Gbolahan <geakande@gmail.com>
2023-08-18 16:27:03 +0100

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -60,20 +60,6 @@ const About = () => {
             If you are looking to build your business/brand or you need an upgrade to a better level, reach out to me and let me make you 
             achieve your business vision.
           </p>
-      <div className="container">
-        <iframe 
-          width="560" 
-          height="315" 
-          src="https://www.youtube.com/embed/RAKNPGBdKoM" 
-          title="YouTube video player" 
-          frameborder="0" 
-          allow="accelerometer; 
-          autoplay; clipboard-write; 
-          encrypted-media; gyroscope; 
-          picture-in-picture; 
-          web-share" allowfullscreen>
-        </iframe>
-      </div>
     </section>
   )
 }
